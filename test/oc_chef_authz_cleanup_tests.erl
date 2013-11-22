ac99ec0e5908770bb40e85766487af79138e1df9
Oliver Ferrigni <oliver@opscode.com>
2013-11-22 11:39:24 -0600

    Leverage bulk delete api in bifrost
    With bulk delete support in bifrost, the cleanup gen_fsm can now send arrays of
    authz_ids to be deleted instead of one request per authz_id.  This endpoint
    returns the count of what was deleted, which we ignore.  This cleanup action
    does not require that the deletion be successful, only that the request is
    made.
    
    Any server or auth error from bifrost will result in the gen_fsm crashing.

--- test/oc_chef_authz_cleanup_tests.erl
+++ test/oc_chef_authz_cleanup_tests.erl
@@ -42,11 +42,12 @@ oc_chef_authz_cleanup_test_() ->
        fun() ->
                oc_chef_authz_cleanup:stop(),
                Actors = to_binary(lists:seq(0,?BATCH_SIZE)),
-               Groups = to_binary(lists:seq(0,?BATCH_SIZE)),
+               Groups = to_binary(lists:seq(?BATCH_SIZE+1,?BATCH_SIZE*2)),
                oc_chef_authz_cleanup:add_authz_ids(Actors, Groups),
-               {StoredActors, _StoredGroups} = oc_chef_authz_cleanup:get_authz_ids(),
+               {StoredActors, StoredGroups} = oc_chef_authz_cleanup:get_authz_ids(),
                {ActorsToBeDeleted, ActorsToBeRemaining} = oc_chef_authz_cleanup:prune(?BATCH_SIZE, sets:to_list(StoredActors)),
-               expect_delete(ActorsToBeDeleted, ActorsToBeDeleted),
+               {GroupsToBeDeleted, GroupsToBeRemaining} = oc_chef_authz_cleanup:prune(?BATCH_SIZE, sets:to_list(StoredGroups)),
+               expect_delete(ActorsToBeDeleted, GroupsToBeDeleted),
                oc_chef_authz_cleanup:prune(),
                {ResultingActorSet, ResultingGroupSet} = oc_chef_authz_cleanup:get_authz_ids(),
                ?assertEqual(ActorsToBeRemaining, sets:to_list(ResultingActorSet)),
@@ -55,8 +56,8 @@ oc_chef_authz_cleanup_test_() ->
       {"prune should remove 1 item from actors and groups",
        fun() ->
                oc_chef_authz_cleanup:stop(),
-               oc_chef_authz_cleanup:add_authz_ids(to_binary([1]),to_binary([1])),
-               expect_delete(to_binary(lists:seq(0, ?BATCH_SIZE-1)), to_binary(lists:seq(0, ?BATCH_SIZE-1))),
+               oc_chef_authz_cleanup:add_authz_ids(to_binary([1]),to_binary([2])),
+               expect_delete(to_binary([1]), to_binary([2])),
                oc_chef_authz_cleanup:prune(),
                {ResultingActorSet, ResultingGroupSet} = oc_chef_authz_cleanup:get_authz_ids(),
                ?assertEqual(sets:from_list([]), ResultingActorSet),
@@ -65,7 +66,7 @@ oc_chef_authz_cleanup_test_() ->
       {"add_authz_ids should delete from bifrost after interval when smaller than batch size",
       fun() ->
               Actors = [ test_utils:make_az_id(integer_to_list(In)) || In <- lists:seq(0,3) ],
-              Groups = [ test_utils:make_az_id(integer_to_list(In)) || In <- lists:seq(0,3) ],
+              Groups = [ test_utils:make_az_id(integer_to_list(In)) || In <- lists:seq(4,6) ],
               expect_delete(Actors, Groups),
               
               oc_chef_authz_cleanup:add_authz_ids(Actors, Groups),
@@ -77,23 +78,24 @@ oc_chef_authz_cleanup_test_() ->
      ]}.
 
 convert_to_path(Actors, Groups) ->
-   ActorBasePath = "actors/",
-   GroupBasePath = "groups/",
-
-   [ActorBasePath ++ binary_to_list(Actor) || Actor <- Actors] ++
-        [GroupBasePath ++ binary_to_list(Group) || Group <- Groups].
+    [lists:sort(Actors), lists:sort(Groups)].
 
 to_binary(List) ->
-    [list_to_binary(integer_to_list(Int)) || Int <- List].
+    [test_utils:make_az_id(integer_to_list(Int)) || Int <- List].
 
 expect_delete(Actors, Groups) ->
     meck:delete(oc_chef_authz_http, request, 5),
     meck:expect(oc_chef_authz_http, request,
-                fun(InputPath, delete, [], [], AzId) ->
-                        PossiblePaths = convert_to_path(Actors, Groups),
-                        FilteredPaths = lists:filter(fun(Elem) ->
-                                                             Elem =:= InputPath
-                                                     end, PossiblePaths),
-                        ?assertEqual([InputPath], FilteredPaths),
-                        ?assertEqual(AzId, ?SUPER_USER_AUTHZ_ID)
+                fun("bulk/delete", post, [], InputBody, AzId) ->
+                        DecodedBody = jiffy:decode(InputBody),
+                        Type = ej:get({<<"type">>}, DecodedBody),
+                        Collection = lists:sort(ej:get({<<"collection">>}, DecodedBody)),
+                        case Type of
+                            <<"actor">> ->
+                                ?assertEqual(Collection, lists:sort(Actors));
+                            <<"group">> ->
+                                ?assertEqual(Collection, lists:sort(Groups))
+                        end,
+                        ?assertEqual(AzId, ?SUPER_USER_AUTHZ_ID),
+                        {ok, ignored_count_json}
                 end).
