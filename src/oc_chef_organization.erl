ba7dc040f42fbbff9634135262738faf4d701f6e
Mark Anderson <mark@opscode.com>
2014-08-04 17:23:46 -0700

    Rework oc_chef_organization to do validation, and add changes needed for organizations endpoint.

--- src/oc_chef_organization.erl
+++ src/oc_chef_organization.erl
@@ -32,13 +32,31 @@
          name/1,
          id/1,
          org_id/1,
-         type_name/1
+         type_name/1,
+         assemble_organization_ejson/1,
+         parse_binary_json/1
         ]).
 
 -mixin([
         {chef_object, [{default_fetch/2, fetch},
                        {default_update/2, update}]}
        ]).
+
+%% We don't have a class for 'organizations' on the client yet, but eventually we may want
+%% to send a json_class Chef::ApiOrganization or the like.
+%% Then we would amend DEFAULT_FIELD_VALUES and VALIDATION_CONSTRAINTS to include:
+%% {<<"json_class">>, <<"Chef::Organization">>},
+%% {<<"chef_type">>, <<"organization">>},
+-define(DEFAULT_FIELD_VALUES, [ ]).
+
+-define(VALIDATION_CONSTRAINTS,
+        [ {<<"name">>, {string_match, regex_for(org_name)}},
+          {<<"full_name">>,  {string_match, regex_for(org_full_name) }}
+         ]).
+
+-define(VALID_KEYS, [<<"name">>, <<"full_name">>]).
+
+
 
 authz_id(#oc_chef_organization{authz_id = AuthzId}) ->
     AuthzId.
@@ -49,8 +67,10 @@ is_indexed() ->
 ejson_for_indexing(#oc_chef_organization{}, _EjsonTerm) ->
    erlang:error(not_indexed).
 
-update_from_ejson(#oc_chef_organization{} = _Organization, _OrganizationData) ->
-    erlang:error(not_implemented).
+update_from_ejson(#oc_chef_organization{} = Organization, OrganizationData) ->
+    Name = ej:get({<<"name">>}, OrganizationData),
+    FullName = ej:get({<<"fullname">>}, OrganizationData),
+    Organization#oc_chef_organization{name = Name, full_name = FullName}.
 
 set_created(#oc_chef_organization{} = Organization, ActorId) ->
     Now = chef_object_base:sql_date(now),
@@ -75,6 +95,7 @@ find_query() ->
 list_query() ->
     list_organizations.
 
+%% Not implemented because we have no serialized json body
 bulk_get_query() ->
     erlang:error(not_implemented).
 
@@ -93,6 +114,28 @@ record_fields() ->
 
 list(#oc_chef_organization{id = Id}, CallbackFun) ->
     CallbackFun({list_query(), [Id], [name]}).
+
+parse_binary_json(Bin) ->
+    Org0 = chef_json:decode_body(Bin),
+    Org = chef_object_base:set_default_values(Org0, ?DEFAULT_FIELD_VALUES),
+    {ok, ValidOrg} = validate_org(Org), %% TODO need action specific version?
+    ValidOrg.
+
+validate_org(Org) ->
+    case ej:valid(?VALIDATION_CONSTRAINTS, Org) of
+        ok -> {ok, Org};
+        Bad -> throw(Bad)
+    end.
+
+%%
+%% Open question: We don't expose json_class and chef_type fields currently, and the client doesn't have objects for them.
+%% Is it worth sending at least chef_type fields?
+%%
+assemble_organization_ejson(#oc_chef_organization{name = Name,
+                                                  full_name = FullName}) ->
+    Org = {[{<<"name">>, Name},
+            {<<"full_name">>, FullName} ]},
+    chef_object_base:set_default_values(Org, ?DEFAULT_FIELD_VALUES).
 
 new_record(null, AuthzId, OrganizationData) ->
     % TODO: write id generator for org-less objects (see chef_object_base:make_org_prefix_id
@@ -121,3 +164,29 @@ org_id(#oc_chef_organization{}) ->
 
 type_name(#oc_chef_organization{}) ->
     organization.
+
+%%
+%% TODO: This was copy-pasta'd from chef_regex, reunite someday (see also oc_chef_container)
+%%
+%% The name regex should limit to some short length. Nginx default is 8k. (large_client_header_buffers)
+%% but we probably for sanity's sake want something less. 255 seems reasonable, but we probably should dig deeper.
+%% For reference, I've succesfully created orgs of 900+ character lengths in hosted, but failed with 1000.
+%% Probably have to be able to handle api.opscode.us/<ORGNAME>/environments/default in 1k?)
+-define(ORG_NAME_REGEX, "[.[:alnum:]_-]{1,255}").
+-define(FULL_NAME_REGEX, "\S.{0,1022}"). %% Must start with nonspace.
+-define(ANCHOR_REGEX(Regex), "^" ++ Regex ++ "$").
+
+generate_regex(Pattern) ->
+  {ok, Regex} = re:compile(Pattern),
+  Regex.
+
+generate_regex_msg_tuple(Pattern, Message) ->
+  Regex = generate_regex(Pattern),
+  {Regex, Message}.
+
+regex_for(org_name) ->
+    generate_regex_msg_tuple(?ANCHOR_REGEX(?ORG_NAME_REGEX),
+                             <<"Malformed org name.  Must only contain A-Z, a-z, 0-9, _, or -">>);
+regex_for(org_full_name) ->
+    generate_regex_msg_tuple(?ANCHOR_REGEX(?FULL_NAME_REGEX),
+                             <<"Malformed org full name.  Must only contain A-Z, a-z, 0-9, _, or -">>).
